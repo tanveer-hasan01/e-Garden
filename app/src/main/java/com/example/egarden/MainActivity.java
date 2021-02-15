d5bef15318e0b93f83ee9d87ba6cc1a029b0bf13
tanveerhasan <tanveer.cse01@gmail.com>
2021-02-15 20:56:16 +0600

    view binding enable & add custom navigation drawer

--- app/src/main/java/com/example/egarden/MainActivity.java
+++ app/src/main/java/com/example/egarden/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.egarden;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.egarden.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private ActivityMainBinding binding;
+    Home homeFragment;
+    Toolbar toolbarr;
+    TextView toolbarTitle;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+
+        toolbarr=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbarr);
+
+        toolbarTitle=findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText(R.string.app_name);
+
+        setSupportActionBar(binding.include.toolbar);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,
+                binding.drawerLayout,binding.include.toolbar,R.string.open, R.string.close);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getColor(R.color.white));
+
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        binding.navigationView.setNavigationItemSelectedListener(this);
+        binding.list.setOnClickListener((View.OnClickListener) this);
+        initFragmentHome();
+
+    }
+
+
+    private void initFragmentHome(){
+        homeFragment=new Home();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(binding.include.contentMain.fragmentContainer.getId(),homeFragment,"HomeFragment").commit();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+        switch (view.getId()) {
+            case R.id.list:
+
+
+        }
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
